3251b7f3b8309ba0cbc5ab71472d4d1c8f01a903
asha <your_email@example.com>
2026-04-15 15:34:41 +0530

    update sellland

--- src/pages/SellLand.jsx
+++ src/pages/SellLand.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import API from "../services/api";
-import { Helmet } from "react-helmet";
+import { Helmet } from "react-helmet-async";
 
 const SellLand = () => {
 
